377dce5c857d2555f3086197d1170de7a430ec9a
Jai <jaimiles23@gmail.com>
2020-08-17 16:11:46 -0700

    added test for csv headers

--- user_class/test_classes.py
+++ user_class/test_classes.py
@@ -2,7 +2,7 @@
  * @author [Jai Miles]
  * @email [jaimiles23@gmail.com]
  * @create date 2020-08-17 15:58:59
- * @modify date 2020-08-17 15:58:59
+ * @modify date 2020-08-17 16:06:50
  * @desc [
     Script to test User class instantiations.
 
@@ -10,18 +10,35 @@
  */
 """
 
+
 ##########
 # Imports
 ##########
 
 from user_constants import UserConstants
-from create_user_class import User, test_user_class, instantiate_user
-from user_saver_class import UserSaver
+from create_user_class import (
+    User, 
+    test_user_class, 
+    instantiate_user
+)
+from user_saver_class import (
+    UserSaver,
+    test_csv_name,
+    
+)
 
 
 ##########
 # Tests
 ##########
+
+def test_csv_headers():
+    """
+    Test if UserSaver.get_csv_headers() functions correctly.
+    """
+    jai = instantiate_user()
+    keys = UserSaver.get_csv_headers(jai)
+    print(keys)
 
 
 
@@ -29,11 +46,14 @@ from user_saver_class import UserSaver
 # Main
 ##########
 
+
+
+
+
 def main():
-    
-    jai = instantiate_user()
-    
+    test_csv_headers()
 
 
 if __name__ == "__main__":
     main()
+
