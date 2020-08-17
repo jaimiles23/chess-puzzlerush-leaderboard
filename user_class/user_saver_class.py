d6f4503735fc22ad056b6307ab237f647285b4ca
Jai <jaimiles23@gmail.com>
2020-08-16 17:42:36 -0700

    innit

---
+++ user_class/user_saver_class.py
@@ -0,0 +1,31 @@
+"""/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-08-16 17:40:01
+ * @modify date 2020-08-16 17:40:01
+ * @desc [
+    Class to save user profiles to csv.
+ ]
+ */
+"""
+
+##########
+# Imports
+##########
+
+from user_class.create_user_class import User
+
+
+
+##########
+# UserSaver class
+##########
+
+class UserSaver(object):
+   """
+   Class with auxiliary methods to save user profiles.
+   """
+
+
+
+    
