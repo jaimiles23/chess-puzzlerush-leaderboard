030db56df66667d0302c6d016aa7d8148fe3a698
Jai <jaimiles23@gmail.com>
2020-08-16 20:53:48 -0700

    added method to get user_info

--- user_class/user_saver_class.py
+++ user_class/user_saver_class.py
@@ -24,16 +24,37 @@ from user_class.user_constants import UserConstants
 
 class UserSaver(UserConstants):
    """
-   Class with auxiliary methods to save user profile
+   Class with auxiliary methods to save user profile.
    """
 
    @staticmethod
-   def get_user_attr_for_csv( User: object) -> list:
+   def format_user_attr_for_csv( User: object) -> list:
       """Returns list of user object attributes for csv format.
+
+      NOTE: Want to somehow place user ID at front of list.
       """
+      user_attr = list()
+
+      user_attr += UserSaver.get_user_info(User) 
+
+
+
+      return user_attr
+
+
+   @staticmethod
+   def get_user_info( User: object) -> list:
+      """Returns list of user info.
       
+      NOTE: player_id is first constant in list.
+      """
+      user_info = []
 
-
+      for key in UserSaver.keys_info:
+         value = User.user_info[key]
+         user_info.append(value)
+      
+      return user_info
 
 
 
