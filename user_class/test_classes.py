69018f88db5755475577fe823540990875b38a12
Jai <jaimiles23@gmail.com>
2020-08-17 16:02:27 -0700

    init

---
+++ user_class/test_classes.py
@@ -0,0 +1,39 @@
+"""/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-08-17 15:58:59
+ * @modify date 2020-08-17 15:58:59
+ * @desc [
+    Script to test User class instantiations.
+
+ ]
+ */
+"""
+
+##########
+# Imports
+##########
+
+from user_constants import UserConstants
+from create_user_class import User, test_user_class, instantiate_user
+from user_saver_class import UserSaver
+
+
+##########
+# Tests
+##########
+
+
+
+##########
+# Main
+##########
+
+def main():
+    
+    jai = instantiate_user()
+    
+
+
+if __name__ == "__main__":
+    main()
