fcc340a95a71e7b8382eca4cdb4fecdc2ca7b016
Jai <jaimiles23@gmail.com>
2020-08-17 18:58:43 -0700

    init

---
+++ logger/logging.py
@@ -0,0 +1,24 @@
+"""/**
+ * @author [Jai Miles]
+ * @email [jaimiles23@gmail.com]
+ * @create date 2020-08-17 18:55:24
+ * @modify date 2020-08-17 18:55:24
+ * @desc [
+    Logger for module
+ ]
+ */
+"""
+
+##########
+# Imports
+##########
+
+from logging import getLogger
+
+
+##########
+# Logger
+##########
+
+logger = getLogger(__name__)
+logger.setLevel(10)
