0dce3b5b48f463743dbbc563eb572e39c8c802d6
Jai <jaimiles23@gmail.com>
2020-08-18 18:15:12 -0700

    moved testing to tests.py in main folder

--- tests.py
+++ tests.py
@@ -1,17 +1,10 @@
 """/**
  * @author [Jai Miles]
  * @email [jaimiles23@gmail.com]
- * @create date 2020-08-16 13:47:49
- * @modify date 2020-08-16 17:39:30
+ * @create date 2020-08-18 18:13:04
+ * @modify date 2020-08-18 18:13:04
  * @desc [
-    Main script to find chess.com ratings for user profiles.
-
-    Steps:
-        1. Imports
-        2. Webscrape puzzle rush leaderboard
-        3. Create user profiles with stats
-        4. Convert user profiles to be saved
-        5. Write to csv
+    Tests for main script.
  ]
  */
 """
@@ -35,16 +28,16 @@ from user_class.user_saver import UserSaver
 logger = logging.getLogger(__name__ + "main")
 
 
-
 ##########
-# Main Scripts
+# Tests
 ##########
 
-def main():
-    
+def test_user_profile_complete() -> bool:
+    """
+    Returns boolean if all users havea  complete profile.
+    """
     logging.info("1.  Webscraping")
     usernames_scores = LeaderboardScraper.get_usernames_scores()
-
 
     logging.info("2.  Create user profiles")
     user_profiles = []
@@ -52,8 +45,28 @@ def main():
         user_profiles.append( User(username, score))
 
 
-    logging.info("3.  Save user profiles")
-    UserSaver.write_users_to_csv(user_profiles)
+    # Determine which vars are NOT included in user attributes.
+    ## 
+    attr_dict = dict()
+    for profile in user_profiles:
+        
+        for v in vars(profile):
+            if v not in attr_dict.keys():
+                attr_dict[v] = 1
+            else:
+                attr_dict[v] = attr_dict[v] + 1
+    
+    for k, v in attr_dict.items():
+        print(k, '-', v)
+        assert v == 50
+
+
+##########
+# Main Scripts
+##########
+
+def main():
+    test_user_profile_complete()
 
 
 ##########
